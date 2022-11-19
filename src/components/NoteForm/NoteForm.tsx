f3050b1ca52db4c45e20302d36feaa3f37a85fbb
Sanket <code.sanket13@gmail.com>
2022-11-19 23:59:30 +0530

    adding submit functionality

--- src/components/NoteForm/NoteForm.tsx
+++ src/components/NoteForm/NoteForm.tsx
@@ -1,14 +1,53 @@
+//importing styles
 import styles from "./NoteForm.module.scss";
+
+//importing react packages
+import { FormEvent, useRef } from "react";
 import CreatableReactSelect from "react-select/creatable";
 import { Link } from "react-router-dom";
-const NoteForm = () => {
+// import { NoteData } from "../../App";
+export type Note = {
+  id: string;
+} & NoteData;
+
+export type NoteData = {
+  title: string;
+  markdown: string;
+  tags: Tag[];
+};
+
+export type Tag = {
+  id: string;
+  label: string;
+};
+//declaring types
+type NoteFormProps = {
+  onSubmit: (data: NoteData) => void;
+};
+
+//jsx
+const NoteForm = ({ onSubmit }: NoteFormProps) => {
+  const titleRef = useRef<HTMLInputElement>(null);
+  const markdownRef = useRef<HTMLTextAreaElement>(null);
+
+  function handleSubmit(e: FormEvent) {
+    e.preventDefault();
+
+    onSubmit({
+      title: titleRef.current!.value,
+      markdown: markdownRef.current!.value,
+      tags: [],
+    });
+  }
+
+  //jsx to return
   return (
     <div className={styles.noteForm}>
-      <form className={styles.form}>
+      <form className={styles.form} onSubmit={handleSubmit}>
         <div className={styles.upper}>
           <div className={styles.searchType}>
             <label htmlFor="">Title</label>
-            <input type="text" className={styles.input} />
+            <input type="text" className={styles.input} ref={titleRef} />
           </div>
           <div className={styles.searchType}>
             <label htmlFor="">Tags</label>
@@ -17,7 +56,7 @@ const NoteForm = () => {
         </div>
 
         <div className={styles.lower}>
-          <textarea name="" id=""></textarea>
+          <textarea name="" id="" ref={markdownRef}></textarea>
           <div className={styles.btnGroup}>
             <button type="submit" className={styles.saveBtn}>
               Save
